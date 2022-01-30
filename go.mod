b073cb3a13d1d72d915371e30c9e49b1bbe3ac1e
David Wittman <dwittman@gmail.com>
2022-01-30 14:24:06 -0600

    Create loggroup and r53 query log

--- go.mod
+++ go.mod
@@ -14,8 +14,11 @@ require (
 	github.com/aws/aws-sdk-go-v2/internal/configsources v1.1.4 // indirect
 	github.com/aws/aws-sdk-go-v2/internal/endpoints/v2 v2.2.0 // indirect
 	github.com/aws/aws-sdk-go-v2/internal/ini v1.3.4 // indirect
+	github.com/aws/aws-sdk-go-v2/service/ec2 v1.29.0 // indirect
 	github.com/aws/aws-sdk-go-v2/service/internal/presigned-url v1.7.0 // indirect
+	github.com/aws/aws-sdk-go-v2/service/route53resolver v1.13.0 // indirect
 	github.com/aws/aws-sdk-go-v2/service/sso v1.9.0 // indirect
 	github.com/aws/aws-sdk-go-v2/service/sts v1.14.0 // indirect
 	github.com/aws/smithy-go v1.10.0 // indirect
+	github.com/jmespath/go-jmespath v0.4.0 // indirect
 )
--- main.go
+++ main.go
@@ -12,35 +12,145 @@ import (
 	"github.com/aws/aws-sdk-go-v2/aws"
 	"github.com/aws/aws-sdk-go-v2/config"
 	"github.com/aws/aws-sdk-go-v2/service/cloudwatchlogs"
+	"github.com/aws/aws-sdk-go-v2/service/ec2"
+	"github.com/aws/aws-sdk-go-v2/service/route53resolver"
+	r53types "github.com/aws/aws-sdk-go-v2/service/route53resolver/types"
 )
 
+const LOG_GROUP_NAME = "/ec2/dnsspy"
+
+func getLogGroupArn(ctx context.Context, cwl *cloudwatchlogs.Client, name string) (*string, error) {
+	resp, err := cwl.DescribeLogGroups(ctx, &cloudwatchlogs.DescribeLogGroupsInput{
+		Limit:              aws.Int32(1),
+		LogGroupNamePrefix: &name,
+	})
+	if err != nil {
+		return aws.String(""), err
+	}
+
+	if len(resp.LogGroups) > 0 {
+		return resp.LogGroups[0].Arn, nil
+	}
+
+	return nil, nil
+}
+
+// ensureLogGroupExists first checks if the log group exists, if it doesn't it will create one.
+// returns the ARN of the log group or an error
+func ensureLogGroupExists(ctx context.Context, cwl *cloudwatchlogs.Client, name string, retentionDays int32) (*string, error) {
+	arn, err := getLogGroupArn(ctx, cwl, name)
+	if err != nil {
+		return aws.String(""), err
+	}
+	if arn != nil {
+		return arn, nil
+	}
+
+	_, err = cwl.CreateLogGroup(ctx, &cloudwatchlogs.CreateLogGroupInput{
+		LogGroupName: &name,
+	})
+	if err != nil {
+		return aws.String(""), err
+	}
+
+	_, err = cwl.PutRetentionPolicy(ctx, &cloudwatchlogs.PutRetentionPolicyInput{
+		RetentionInDays: aws.Int32(retentionDays),
+		LogGroupName:    &name,
+	})
+
+	return getLogGroupArn(ctx, cwl, name)
+}
+
+func ensureResolverQueryLogExists(ctx context.Context, r53 *route53resolver.Client, logGroupArn, vpcID *string) error {
+	// TODO handle if there are > 1 or something, multi region?
+	//      it might be better to just check for this w/ an associated vpc id before ensuring the cw logs?
+	list, err := r53.ListResolverQueryLogConfigs(ctx, &route53resolver.ListResolverQueryLogConfigsInput{
+		//
+		Filters: []r53types.Filter{
+			r53types.Filter{
+				Name:   aws.String("Name"),
+				Values: []string{"ec2-dnsspy"},
+			},
+		},
+	})
+	if err != nil {
+		return err
+	}
+	// TODO handle > 1
+	if list.TotalCount > 0 {
+		return nil
+	}
+	resp, err := r53.CreateResolverQueryLogConfig(ctx, &route53resolver.CreateResolverQueryLogConfigInput{
+		CreatorRequestId: aws.String("todothisshouldbeunique"),
+		DestinationArn:   logGroupArn,
+		Name:             aws.String("ec2-dnsspy"),
+	})
+	if err != nil {
+		return err
+	}
+	_, err = r53.AssociateResolverQueryLogConfig(ctx, &route53resolver.AssociateResolverQueryLogConfigInput{
+		ResolverQueryLogConfigId: resp.ResolverQueryLogConfig.Id,
+		ResourceId:               vpcID,
+	})
+	return err
+}
+
+func setup(ctx context.Context, cfg aws.Config, instanceID string) error {
+	fmt.Println("Looking up VPC ID")
+	ec2c := ec2.NewFromConfig(cfg)
+	resp, err := ec2c.DescribeInstances(ctx, &ec2.DescribeInstancesInput{
+		InstanceIds: []string{instanceID},
+	})
+	if err != nil {
+		return err
+	}
+	vpcID := resp.Reservations[0].Instances[0].VpcId
+
+	fmt.Println("Creating Cloudwatch log group")
+	cw := cloudwatchlogs.NewFromConfig(cfg)
+	logGroupArn, err := ensureLogGroupExists(ctx, cw, LOG_GROUP_NAME, 1)
+	if err != nil {
+		return err
+	}
+
+	fmt.Println("Creating Route53 resolver query log")
+	r53 := route53resolver.NewFromConfig(cfg)
+	err = ensureResolverQueryLogExists(ctx, r53, logGroupArn, vpcID)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
 func main() {
-	var instanceID, logGroup string
+	var instanceID string
 
 	flag.StringVar(&instanceID, "i", "", "EC2 Instance ID")
 	flag.StringVar(&instanceID, "instance-id", "", "EC2 Instance ID")
-	flag.StringVar(&logGroup, "l", "", "Cloudwatch Logs Log Group")
-	flag.StringVar(&logGroup, "log-group", "", "Cloudwatch Logs Log Group")
 	flag.Usage = func() {
 		fmt.Print(`Spy on DNS requests for your EC2 instances in (almost) real-time
 
 usage: ec2-dnsspy [options]
 	-i, --instance-id	EC2 Instance ID
-	-l, --log-group		Cloudwatch Logs Log Group
 `)
 	}
 	flag.Parse()
 
-	// TODO (check flags are set)
-
-	cfg, err := config.LoadDefaultConfig(context.TODO())
+	ctx := context.TODO()
+	cfg, err := config.LoadDefaultConfig(ctx)
 	if err != nil {
 		panic("couldn't configure aws client")
 	}
-	client := cloudwatchlogs.NewFromConfig(cfg)
+
+	err = setup(ctx, cfg, instanceID)
+	if err != nil {
+		panic(err)
+	}
+	cw := cloudwatchlogs.NewFromConfig(cfg)
 
 	tailConfig := TailConfig{
-		LogGroupName:  aws.String(logGroup),
+		LogGroupName:  aws.String(LOG_GROUP_NAME),
 		LogStreamName: aws.String("."),
 		StartTime:     aws.Time(time.Now()),
 		EndTime:       aws.Time(time.Now()),
@@ -57,7 +167,7 @@ usage: ec2-dnsspy [options]
 		}
 	}()
 
-	tail, err := Tail(client, tailConfig, limiter, log.New(ioutil.Discard, "", 0))
+	tail, err := Tail(cw, tailConfig, limiter, log.New(ioutil.Discard, "", 0))
 	if err != nil {
 		panic(err)
 	}
